1ff9863cf1775c00d17a3884db1d016647480a81
scott <scottsvance7@gmail.com>
2022-07-20 11:39:08 -0400

    add add reimbursement page to frontend

---
+++ frontend/add-reimb.js
@@ -0,0 +1,47 @@
+let addButton = document.getElementById("submit-btn");
+let amount = document.getElementById('amount');
+let description = document.getElementById('description');
+let author = document.getElementById('author');
+let typeButtons = document.querySelectorAll('input[name="type"]')
+
+
+addButton.addEventListener('click', async (e) => {
+    let selectedTypeButton;
+    for (let radioBtn of typeButtons) {
+        if (radioBtn.checked) {
+            selectedTypeButton = radioBtn
+            break;
+        }
+    }
+    e.preventDefault();
+    try {
+        let res = await fetch('http://127.0.0.1:8080/users/1/reimbursements', {
+            'credentials': 'include',
+            'method': 'POST',
+            'headers': {
+                'Content-Type': 'application/json'},
+                'body': JSON.stringify({
+                    "reimbursement_amount": amount.value,
+                    "type": selectedTypeButton.value,
+                    "description": description.value,
+                    "author": author.value
+                })})
+        if (res.status == 201) {
+            window.location.href="./user.html"
+        } else {
+            console.log("Not 201");
+        }
+        
+        // let data = await res.json();
+
+        // addReimbursement(data);
+        
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+// function addReimbursement(reimb) {
+    
+    
+// };
--- frontend/user.js
+++ frontend/user.js
@@ -1,13 +1,24 @@
-let logoutButton = document.getElementById('logout-button')
+let logoutButton = document.getElementById('logout-button');
 
-document.addEventListener('DOMContentLoaded', async function loadReimbursements() {
+let reimbursementTbodyElement = document.getElementById('reimb-table-body');
+
+let addReimbursementButton = document.getElementById('add-reimb-btn');
+
+document.addEventListener('DOMContentLoaded', async (e) => {
+
     console.log("Hello There")
     try {
-        let res = await fetch(`http://127.0.0.1:8080/users/1/reimbursements`);
+        let res = await fetch(`http://127.0.0.1:8080/users/1/reimbursements`, {
+        'credentials': 'include',
+        'method': 'GET',
+        'headers': {
+            'Content-Type': 'application/json'}});
+        
 
         let data = await res.json();
+        console.log(data)
 
-        addReimbursementsToTable(data);
+        addReimbursementsToTable(data.reimbursements);
     } catch(err) {
         console.log(err);
     }
@@ -31,36 +42,49 @@ logoutButton.addEventListener('click', async (e) => {
     }
 });
 
-function addReimbursementsToTable(reimb) {
-    let reimbursementTbodyElement = document.getElementById('reimb-table-body');
+function addReimbursementsToTable(reimb_obj) {
+    console.log(typeof(reimb_obj))
+      for (reimb of reimb_obj) {  
+        let row = document.createElement('tr');
 
-    let row = document.createElement('tr');
+        let idCell = document.createElement('td');
+        idCell.innerHTML = reimb.reimb_id
+        let amntCell = document.createElement('td');
+        amntCell.innerHTML = reimb.amount
+        let submitCell = document.createElement('td');
+        submitCell.innerHTML = reimb.submitted;
+        let submitByCell = document.createElement('td');
+        submitByCell.innerHTML = reimb.author
+        let resolvedCell = document.createElement('td');
+        resolvedCell.innerHTML = reimb.resolved;
+        let resolvedByCell = document.createElement('td');
+        resolvedByCell.innerHTML = reimb.resolver
+        let statusCell = document.createElement('td');
+        statusCell.innerHTML = reimb.status
+        let typeCell = document.createElement('td');
+        typeCell.innerHTML = reimb.type
+        let descCell = document.createElement('td');
+        descCell.innerHTML = reimb.description
+        let receiptCell = document.createElement('td');
+        receiptCell.innerHTML = reimb.receipt
 
-    let idCell = document.createElement('td');
-    idCell.innerHTML = reimb.reimb_id
-    let amntCell = createElement('td');
-    amntCell.innerHTML = reimb.amount
-    let submitByCell = createElement('td');
-    submitByCell.innerHTML = reimb.author
-    let resolvedByCell = createElement('td');
-    resolvedByCell.innerHTML = reimb.resolver
-    let statusCell = createElement('td');
-    statusCell.innerHTML = reimb.status
-    let typeCell = createElement('td');
-    typeCell.innerHTML = remb.type
-    let descCell = createElement('td');
-    descCell.innerHTML = reimb.description
-    let receiptCell = createElement('td');
-    receiptCell.innerHTML = reimb.receipt
+        row.appendChild(idCell);
+        row.appendChild(amntCell);
+        row.appendChild(submitCell);
+        row.appendChild(submitByCell);
+        row.appendChild(resolvedCell);
+        row.appendChild(resolvedByCell);
+        row.appendChild(statusCell);
+        row.appendChild(typeCell);
+        row.appendChild(descCell);
+        row.appendChild(receiptCell);
 
-    row.appendChild(idCell);
-    row.appendChild(amntCell);
-    row.appendChild(submitByCell);
-    row.appendChild(resolvedByCell);
-    row.appendChild(statusCell);
-    row.appendChild(typeCell);
-    row.appendChild(descCell);
-    row.appendChild(receiptCell);
-
-    reimbursementTbodyElement.appendChild(row);
+        reimbursementTbodyElement.appendChild(row);
+    }
 };
+
+addReimbursementButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+
+    window.location.href="./add-reimb.html"});
